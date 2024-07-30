35e65e41c6a04e5c5f6cc7a7ec4a296347a6187e
nitin-1603 <ch.nitin48@gmail.com>
2024-07-30 17:20:55 +0530

    from office 30.07.2024

--- src/components/childcomponents/EffectHook.jsx
+++ src/components/childcomponents/EffectHook.jsx
@@ -1,13 +1,80 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import '../../css components/EffectHook.css'
 const EffectHook = () => {
+    const [apiData, setApiData] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const url = 'https://jsonplaceholder.typicode.com/users';
+            const fetchData = await fetch(url)
+            const data = await fetchData.json();
+            setApiData(data)
+        }
+        fetchData();
+    }, [])
 
 
     return (
 
         <>
-        
+            <div className='containerTable'>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th >Address
+                           
+                                <th >Street</th>
+                                <th>Suite</th>
+                                <th>City</th>
+                                <th>Zipcode</th>
+                           
+                        </th>
+                        <th>phone</th>
+                        <th>Website</th>
+                        <th>Company
+                            
+                                <th>Name</th>
+                                <th>Catch Phrase</th>
+                                <th>BS</th>
+                           
+                        </th>
+                    </tr>
 
+
+                    {apiData.map((item) => {
+                        return (
+                            <tr>
+                                <th>{item.id}</th>
+                                <th>{item.name}</th>
+                                <th>{item.username}</th>
+                                <th>{item.email}</th>
+                                <th >
+                                    
+                                        <th >{item.address.street}</th>
+                                        <th>{item.address.suite}</th>
+                                        <th>{item.address.city}</th>
+                                        <th>{item.address.zipcode}</th>
+                                    
+                                </th>
+                                <th>{item.phone}</th>
+                                <th>{item.website}</th>
+                                <th>
+                                    
+                                        <th>{item.company.name}</th>
+                                        <th>{item.company.catchPhrase}</th>
+                                        <th>{item.company.bs}</th>
+                                    
+                                </th>
+
+                            </tr>
+                        )
+                    })}
+
+                </table>
+            </div>
         </>
     )
 }
